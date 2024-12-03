e015eecc81ee8229c237a165ac230d79e5ef7238
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-12-03 18:50:45 -0500

    Read and print contents of 'content/index.md'

---
+++ src/groups.py
@@ -0,0 +1,12 @@
+import os
+
+
+markdown_filename = os.path.expanduser(
+    "~/boot_dev/Static_Site_Generator/content/index.md"
+)
+
+
+with open(markdown_filename, "r", encoding="utf-8") as markdown_file:
+    markdown_text = markdown_file.read()
+
+    print(markdown_text)

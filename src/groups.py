04765d93825ccb0cd63e805dbb4faf899d776e2e
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-12-03 19:32:38 -0500

    Print Markdown file contents by nickname based on filename
    That is, we implement a function that does this.

--- src/groups.py
+++ src/groups.py
@@ -1,12 +1,15 @@
 import os
 
 
-markdown_filename = os.path.expanduser(
-    "~/boot_dev/Static_Site_Generator/content/tolkien.md"
-)
+def get_markdown_file_content(nickname: str) -> str:
+    markdown_filename = os.path.expanduser(
+        f"~/boot_dev/Static_Site_Generator/content/{nickname}.md"
+    )
+
+    with open(markdown_filename, "r", encoding="utf-8") as markdown_file:
+        markdown_text = markdown_file.read()
+
+        return markdown_text
 
 
-with open(markdown_filename, "r", encoding="utf-8") as markdown_file:
-    markdown_text = markdown_file.read()
-
-    print(markdown_text)
+print(get_markdown_file_content("original_example"))

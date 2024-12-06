79ba5fddb7b2c9264198617250ae33739383a78e
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-12-05 21:49:12 -0500

    Add more sophisticated test for 'process_word_group'
    This one reveals a small bug regarding punctuation, but the fix (if it
    should be needed) might not require too much refactoring.

--- src/test_groups.py
+++ src/test_groups.py
@@ -10,3 +10,10 @@ class TestProcessWordGroup(unittest.TestCase):
         self.assertEqual(processed, "<b>This</b>")
 
         print(processed)
+
+    def test_multiple_simple_cases(self):
+        group = ["This", "**", "is", "**", "a", "*", "line", "*", ":", "`", "hello", "world", "`"]
+        processed = src.process_word_group(group)
+
+        # Note that this represents a slight bug in our program.
+        self.assertEqual("This <b>is</b> a <i>line</i> : <code>hello world</code>", processed)

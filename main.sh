d2ff0fe11dfbea95979581799f7f94b5c0267718
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-12-06 13:45:11 -0500

    Expand shell-script functionality

--- main.sh
+++ main.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 export PYTHONPATH="./src"
+default_nickname="index"
+public_dir="$HOME"/boot_dev/Static_Site_Generator/public
 
-python3 "$HOME"/boot_dev/Static_Site_Generator/src/main.py
+mkdir "$public_dir"
+python3 "$HOME"/boot_dev/Static_Site_Generator/src/main.py $default_nickname "$public_dir"/index.html
+
 # cd public && python3 -m http.server 8888

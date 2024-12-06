ca2e3f1b082a6515a1b975285e9f07720ba8e0eb
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-12-06 11:55:57 -0500

    Updated tests to reflect bugfixes in source
    That is, now the tests can be less shy about requiring sane output
    from our functions.

--- src/test_groups.py
+++ src/test_groups.py
@@ -10,32 +10,54 @@ class TestProcessWordGroup(unittest.TestCase):
         self.assertEqual(processed, "<b>This</b>")
 
 
+    def test_without_markup(self):
+        group = ['In', ' ', 'the', ' ', 'beginning.']
+        expected = "In the beginning."
+        actual = src.process_word_group(group)
+
+        self.assertEqual(expected, actual)
+
+
     def test_multiple_simple_cases(self):
-        group = ["This", "**", "is", "**", "a", "*", "line", "*", ":", "`", "hello", "world", "`"]
+        group = [
+            'This', ' ', 'is', ' ', 'a', ' ', 'paragraph', ' ', 'of', ' ',
+            'text.', ' ', 'It', ' ', 'has', ' ', 'some', ' ', '**', 'bold',
+            '**', ',', ' ', 'and', ' ', '*', 'italic', '*', ',', ' ', 'words...'
+        ]
+
         processed = src.process_word_group(group)
 
         # Note that this represents a slight bug in our program.
-        self.assertEqual("This <b>is</b> a <i>line</i>: <code>hello world</code>", processed)
+        self.assertEqual("This is a paragraph of text. It has some <b>bold</b>, and <i>italic</i>, words...", processed)
+
 
     def test_mapping_over_list(self):
         groups = [
-            ['This', 'is', 'a', 'paragraph', 'of', 'text.', 'It', 'has', 'some', '**', 'bold', '**', 'and', '*', 'italic', '*', 'words'],
-            ['inside', 'of', 'it.', 'This', 'is', 'more', 'of', 'the', '*', 'same', 'paragraph.', '*', 'This', 'is', 'to', 'prove', 'the'],
-            ['point', 'that', 'paragraphs', 'can', 'span', 'multiple', 'lines.']
+            ['This', ' ', 'is', ' ', 'a', ' ', 'paragraph', ' ', 'of', ' ', 'text.',
+             ' ', 'It', ' ', 'has', ' ', 'some', ' ', '**', 'bold', '**', ',', ' ', 'and',
+             ' ', '*', 'italic', '*', ',', ' ', 'words'],
+            ['inside', ' ', 'of', ' ', 'it.', ' ', 'This', ' ', 'is', ' ', 'more', ' ', 'of',
+             ' ', 'the', ' ', '*', 'same', ' ', 'paragraph', '*', '.', ' ', 'This', ' ', 'is', ' ', 'to', ' ',
+             'prove', ' ', 'the'],
+            ['point', ' ', 'that', ' ', 'paragraphs', ' ', 'can', ' ', 'span', ' ', 'multiple',
+             ' ', 'lines.']
         ]
 
         actual = list(map(src.process_word_group, groups))
         expected = [
-            "This is a paragraph of text. It has some <b>bold</b> and <i>italic</i> words",
-            "inside of it. This is more of the <i>same paragraph.</i> This is to prove the",
+            "This is a paragraph of text. It has some <b>bold</b>, and <i>italic</i>, words",
+            "inside of it. This is more of the <i>same paragraph</i>. This is to prove the",
             "point that paragraphs can span multiple lines."
         ]
 
         self.assertEqual(expected, actual)
 
+
     def test_nested_inline_styles(self):
-        group = ["Dillinger", "is", "a", "cloud-enabled,", "*", "mobile-", "**", "ready", "**", "*", ",", "offline-storage", "compatible..."]
-        actual = src.process_word_group(group)
+        sentence = "Dillinger is a cloud-enabled, <i>mobile-<b>ready</b></i> , offline-storage compatible..."
+        splitted = src.split_keep_whitespace(sentence)
+        tokenized = src.tokenize_inline_style_markers(splitted)
+        rejoined = src.process_word_group(tokenized)
 
         expected = "Dillinger is a cloud-enabled, <i>mobile-<b>ready</b></i> , offline-storage compatible..."
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected, rejoined)

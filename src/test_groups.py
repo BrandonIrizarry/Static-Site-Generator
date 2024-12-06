c67a2521db38a140780b2a8aa68df96d894f874b
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-12-05 22:17:51 -0500

    Add more 'process_word_group' tests

--- src/test_groups.py
+++ src/test_groups.py
@@ -16,3 +16,26 @@ class TestProcessWordGroup(unittest.TestCase):
 
         # Note that this represents a slight bug in our program.
         self.assertEqual("This <b>is</b> a <i>line</i> : <code>hello world</code>", processed)
+
+    def test_mapping_over_list(self):
+        groups = [
+            ['This', 'is', 'a', 'paragraph', 'of', 'text.', 'It', 'has', 'some', '**', 'bold', '**', 'and', '*', 'italic', '*', 'words'],
+            ['inside', 'of', 'it.', 'This', 'is', 'more', 'of', 'the', '*', 'same', 'paragraph.', '*', 'This', 'is', 'to', 'prove', 'the'],
+            ['point', 'that', 'paragraphs', 'can', 'span', 'multiple', 'lines.']
+        ]
+
+        actual = list(map(src.process_word_group, groups))
+        expected = [
+            "This is a paragraph of text. It has some <b>bold</b> and <i>italic</i> words",
+            "inside of it. This is more of the <i>same paragraph.</i> This is to prove the",
+            "point that paragraphs can span multiple lines."
+        ]
+
+        self.assertEqual(expected, actual)
+
+    def test_nested_inline_styles(self):
+        group = ["Dillinger", "is", "a", "cloud-enabled,", "*", "mobile-", "**", "ready", "**", "*", ",", "offline-storage", "compatible..."]
+        actual = src.process_word_group(group)
+
+        expected = "Dillinger is a cloud-enabled, <i>mobile-<b>ready</b></i> , offline-storage compatible..."
+        self.assertEqual(expected, actual)
